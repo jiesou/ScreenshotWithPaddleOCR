bc28d209572918ba6d294e01fe3d0129fdda8cba
jiesou <jiechushou@gmail.com>
2023-01-26 14:33:39 +0800

    fix shell start Docker

--- screenshot.sh
+++ screenshot.sh
@@ -7,7 +7,7 @@ gnome-screenshot -a -f $temp.png
 # If the docker container is not running, start it.
 if  [[ ! $(docker ps -q -f name=^paddleocr$) ]];  then
     notify-send -e "OCR" "Starting docker container"
-    docker run --name paddleocr -idt paddleocr
+    docker run --name paddleocr -idt paddleocr | docker start paddleocr
 fi
 
 docker cp $temp paddleocr:/app
