5e27142a9d16592c03678a184c0662b2949af491
jiesou <jiechushou@gmail.com>
2023-01-30 20:18:10 +0800

    lots of optimizations

--- screenshot.py
+++ screenshot.py
@@ -7,27 +7,30 @@ import pyperclip
 
 # parse args
 parser = argparse.ArgumentParser(description="Screenshot with OCR, see https://github.com/jiesou/ScreenshotWithPaddleOCR")
-parser.add_argument("-l", "--lang", default="ch",
-    help="set ocr language")
+parser.add_argument("-l", "--lang", default="ch", help="set ocr language")
 args = parser.parse_args()
+
+def sendNotify(msg):
+    subprocess.run(["notify-send", "-e", "OCR", msg])
+
+
+client = docker.from_env()
+
+# get docker container
+if client.containers.list(all=True, filters={'name': 'paddleocr'}):
+    container =  client.containers.get('paddleocr')
+else:
+    sendNotify("Starting docker container...")
+    container =  client.containers.run("paddleocr", detach=True, tty=True, name="paddleocr")
+
+if container.status != "running":
+    container.start()
 
 temp = "/tmp/temp.png"
 
 # use gnome screenshot to capture image
 subprocess.run(["gnome-screenshot", "-a", "-f", temp])
 
-client = docker.from_env()
-
-# if container is not running, start it
-if not client.containers.list(filters={'name': 'paddleocr'}):
-    subprocess.run(["notify-send", "-e", "OCR", "Starting docker container"])
-    try:
-        client.containers.run("paddleocr", detach=True, tty=True, name="paddleocr")
-    except docker.errors.APIError:
-        client.containers.get('paddleocr').start()
-
-# get docker container
-container = client.containers.get('paddleocr')
 
 # tar image to send to docker container
 stream = io.BytesIO()
@@ -35,7 +38,10 @@ with tarfile.open(fileobj=stream, mode='w') as tar, open(temp, 'rb') as f:
     info = tar.gettarinfo(fileobj=f)
     info.name = os.path.basename(temp)
     tar.addfile(info, f)
-container.put_archive("/app", stream.getvalue())
+try:
+    container.put_archive("/app", stream.getvalue())
+except AttributeError:
+    exit()
 
 # run OCR
 result = container.exec_run(f'python3 -m ocr {args.lang}').output.decode('utf-8')
@@ -43,5 +49,4 @@ result = container.exec_run(f'python3 -m ocr {args.lang}').output.decode('utf-8'
 # copy OCR result to clipboard
 pyperclip.copy(re.sub(r"\s*$", r"", result))
 
-# send notification
-subprocess.run(["notify-send", "-e", "OCR", "Copied to clipboard"])
+sendNotify("Copied to clipboard")
